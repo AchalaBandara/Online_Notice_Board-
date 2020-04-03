07e304f32111b710c835ec7da01eb3effd53cfac
Achala Bandara <achpavi14@gmail.com>
2020-04-03 12:12:01 +0530

    Update index.php

--- my-mini-project/admin/index.php
+++ my-mini-project/admin/index.php
@@ -13,7 +13,6 @@ header('location:login.php');
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
@@ -24,15 +23,8 @@ header('location:login.php');
 
     <!-- Bootstrap core CSS -->
     <link href="../css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="../css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
     <link href="../css/dashboard.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="../js/ie-emulation-modes-warning.js"></script>
 
     
@@ -51,9 +43,9 @@ header('location:login.php');
 	    
 		
 			<li><a href="index.php?page=update_password"> Update Password</a></li>
-            <li><a href="index.php?page=add_notice">Add New Notices</a></li>
-			 <li><a href="index.php?page=notices"> Manage Notices</a></li>
-            <li><a href="index.php?page=logout">Logout</a></li>
+      <li><a href="index.php?page=add_notice">Add New Notices</a></li>
+			<li><a href="index.php?page=notices"> Manage Notices</a></li>
+      <li><a href="index.php?page=logout">Logout</a></li>
 			
           </ul>
          
@@ -140,9 +132,7 @@ header('location:login.php');
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../js/vendor/jquery.min.js"><\/script>')</script>
     <script src="../js/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
     <script src="../js/vendor/holder.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
